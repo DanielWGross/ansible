05c2516b1f3734c5e4910df6cf7723d954ef37cf
Dan Gross <danwgross@gmail.com>
2023-05-01 17:24:16 -0400

    update

--- tasks/scripts/install-homebrew.sh
+++ tasks/scripts/install-homebrew.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
